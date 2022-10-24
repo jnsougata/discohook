25d21680d3ee4c706d7b275672d941596cf0b767
jnsougata <jnsougata@gmail.com>
2022-10-25 02:03:04 +0530

    updates

--- discohook/interaction.py
+++ discohook/interaction.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from .embed import Embed
 from .component import Components
+import aiohttp
+
+MISSING = object()
 
 
 class CommandData(BaseModel):
@@ -53,17 +56,15 @@ class Interaction(BaseModel):
             supress_embeds: Optional[bool] = False,
     ) -> JSONResponse:
         payload = {}
-        embeds_container = []
-        attachments_container = []
         flag_value = 0
         if embed:
-            embeds_container.append(embed)
-        if embeds:
-            embeds_container.extend(embeds)
+            if not embeds:
+                embeds = []
+            embeds.append(embed)
         if file:
-            attachments_container.append(file)
-        if files:
-            attachments_container.extend(files)
+            if not files:
+                files = []
+            files.append(file)
         if ephemeral:
             flag_value |= 1 << 6
         if supress_embeds:
@@ -72,21 +73,61 @@ class Interaction(BaseModel):
             payload["content"] = str(content)
         if tts:
             payload["tts"] = True
-        if embeds_container:
-            payload["embeds"] = [embed.to_json() for embed in embeds_container]
+        if embeds:
+            payload["embeds"] = [embed.to_json() for embed in embeds]
         if components:
             payload["components"] = components.to_json()
             for component in components._items:  # noqa
                 self.app.ui_factory[component.custom_id] = component
-        if attachments_container:
-            payload["attachments"] = attachments_container
+        if files:
+            payload["attachments"] = files
         if flag_value:
             payload["flags"] = flag_value
 
         return JSONResponse(
             {
-                "type": callback_types.channel_message_with_source.value,
-                "data": payload
+                "data": payload, "type": callback_types.channel_message_with_source.value,
+            },
+            status_code=200
+        )
+
+    def edit(
+            self,
+            content: Optional[str] = MISSING,
+            *,
+            embed: Optional[Embed] = MISSING,
+            embeds: Optional[List[Embed]] = MISSING,
+            components: Optional[Components] = MISSING,
+            tts: Optional[bool] = MISSING,
+            file: Optional[Dict[str, Any]] = MISSING,
+            files: Optional[List[Dict[str, Any]]] = MISSING,
+            supress_embeds: Optional[bool] = MISSING,
+    ):
+        data = {}
+        if embed is None:
+            embeds = []
+        if file is None:
+            files = []
+        if components is None:
+            components = []
+        if content is not MISSING:
+            data["content"] = str(content)
+        if tts is not MISSING:
+            data["tts"] = tts
+        if embeds is not MISSING:
+            data["embeds"] = [embed.to_json() for embed in embeds]
+        if components is not MISSING:
+            data["components"] = components.to_json() if components else []
+            if components:
+                for component in components._items: # noqa
+                    self.app.ui_factory[component.custom_id] = component
+        if files is not MISSING:
+            data["attachments"] = files
+        if supress_embeds is not MISSING:
+            data["flags"] = 1 << 2
+        return JSONResponse(
+            {
+                "data": data, "type": callback_types.update_message.value,
             },
             status_code=200
         )
