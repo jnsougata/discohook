2ce5e2ae5db20a5de58f1bf2c4b01aeafe151191
jnsougata <jnsougata@gmail.com>
2022-11-29 20:26:07 +0530

    version bump `0.0.2`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="discohook",
-    version="0.0.1",
+    version="0.0.2",
     description="discord webhook wrapper for serverless apps",
     url="https://github.com/jnsougata/discohook",
     author="jnsougata",
