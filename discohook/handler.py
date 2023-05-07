23f2fcd8dcec39412b4bccbd1c10c5cc690131ff
imp <106537315+imptype@users.noreply.github.com>
2023-05-07 12:38:18 +0100

    Return response if custom error handler is used

--- discohook/handler.py
+++ discohook/handler.py
@@ -102,7 +102,11 @@ async def handler(request: Request):
             return JSONResponse({"message": "unhandled interaction type"}, status_code=300)
     except Exception as e:
         if request.app.error_handler:
-            await request.app.error_handler(interaction, e)
+            response = await request.app.error_handler(interaction, e)
+            if isinstance(response, Response):
+              return response
+            else:
+              return Response(status_code=500)
         else:
             err = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
             raise RuntimeError(err) from None
