15b83d00f8584c831f7992c044480488cb2a0bfb
jnsougata <jnsougata@gmail.com>
2023-07-10 11:17:56 -0700

    Updates

--- discohook/dash.py
+++ discohook/dash.py
@@ -6,7 +6,7 @@ async def dashboard():
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>discohook | sync</title>
+        <title>Dashboard</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
