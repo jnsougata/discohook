003a7856c9d3a72f3416c6b94512d8479c97e308
jnsougata <jnsougata@gmail.com>
2023-08-04 07:49:44 +0530

    Minor changes & bug fixes

--- discohook/abc.py
+++ discohook/abc.py
@@ -11,7 +11,10 @@ class Interactable:
         self.checks: List[Callable[["Interaction"], bool]] = []
         self._error_handler: Optional[Callable[["Interaction", Exception], Any]] = None
 
-    def add_check(self):
+    def check(self):
+        """
+        A decorator that adds a check to a specific command or component.
+        """
         def decorator(coro: Callable[["Interaction"], bool]):
             if not asyncio.iscoroutinefunction(coro):
                 raise TypeError("check must be a coroutine")
@@ -20,7 +23,9 @@ class Interactable:
         return decorator
 
     def error_handler(self):
-
+        """
+        A decorator that adds an error handler to a specific command or component.
+        """
         def decorator(coro: Callable[["Interaction", Exception], None]):
             if not asyncio.iscoroutinefunction(coro):
                 raise TypeError("error handler must be a coroutine")
