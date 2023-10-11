625edc60b4133f0a909bc1f90a24de7c0c2aa7c3
imp <106537315+imptype@users.noreply.github.com>
2023-10-11 17:54:07 +0100

    fix type

--- discohook/utils.py
+++ discohook/utils.py
@@ -23,7 +23,7 @@ def color_parser(color: Union[int, str]) -> int:
 
 def snowflake_time(snowflake_id):
     discord_epoch = 1420070400000
-    return ((snowflake_id >> 22) + discord_epoch) / 1000
+    return ((int(snowflake_id) >> 22) + discord_epoch) / 1000
 
 
 def try_description(name: str, description: Any, callback: AsyncFunc) -> str:
