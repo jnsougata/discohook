acff69e0d9715dde3b0111c81626792bf5fb4c4f
jnsougata <jnsougata@gmail.com>
2024-03-05 15:17:42 +0530

    ...

--- sample/commands.py
+++ sample/commands.py
@@ -6,40 +6,10 @@ import discohook
 command_tree = discohook.CommandTree()
 
 
-async def exec_and_respond(i: discohook.Interaction, content: str):
-    from io import StringIO
-    import re
-    import sys
-    import os
-
-    os.environ.clear()
-
-    await i.response.defer()
-
-    view = discohook.View()
-    view.add_buttons(delete_button)
-
-    pattern = re.compile("```(?:python|py)?\n([\s\S]*?)\n```")  # noqa
-    code = pattern.search(content)
-    if not code:
-        return await i.response.followup("No code to execute.")
-    orig = sys.stdout
-    sys.stdout = stdout = StringIO()
-    try:
-        exec(f'async def _a_exec(): ' + "".join(f"\n {line}" for line in code.group(1).split("\n")))
-        await locals()["_a_exec"]()
-        sys.stdout = orig
-    except Exception as err:
-        await i.response.followup(f"```py\n{err}\n```", view=view)
-    else:
-        value = stdout.getvalue()
-        if len(value) > 2000:
-            file = discohook.File("output.txt", content=value.encode("utf-8"))
-            return await i.response.followup(file=file, view=view)
-        embed = discohook.Embed("Output")
-        embed.set_author(name=str(i.author), icon_url=i.author.avatar.url)
-        embed.description = f"```\n{value}\n```"
-        await i.response.followup(embed=embed, view=view)
+@command_tree.preload_component("regenerate")
+@discohook.button.new("Regenerate")
+async def regenerate_button(i: discohook.Interaction):
+    await i.response.update_message(embed=make_random_color_card(i))
 
 
 @command_tree.preload_component("delete")
@@ -58,12 +28,6 @@ def make_random_color_card(i: discohook.Interaction) -> discohook.Embed:
     embed.set_image(f"https://singlecolorimage.com/get/{color_hex}/1280x720")
     embed.set_author(name=i.author.name, icon_url=i.author.avatar.url)
     return embed
-
-
-@command_tree.preload_component("regenerate")
-@discohook.button.new("Regenerate")
-async def regenerate_button(i: discohook.Interaction):
-    await i.response.update_message(embed=make_random_color_card(i))
 
 
 @command_tree.slash()
@@ -98,6 +62,42 @@ async def avatar(i: discohook.Interaction, user: discohook.User):
     embed = discohook.Embed()
     embed.set_image(user.avatar.url)
     await i.response.send(embed=embed)
+
+
+async def exec_and_respond(i: discohook.Interaction, content: str):
+    from io import StringIO
+    import os
+    import re
+    import sys
+
+    os.environ.clear()
+
+    await i.response.defer()
+
+    view = discohook.View()
+    view.add_buttons(delete_button)
+
+    pattern = re.compile("```(?:python|py)?\n([\s\S]*?)\n```")  # noqa
+    code = pattern.search(content)
+    if not code:
+        return await i.response.followup("No code to execute.")
+    orig = sys.stdout
+    sys.stdout = stdout = StringIO()
+    try:
+        exec(f'async def _a_exec(): ' + "".join(f"\n {line}" for line in code.group(1).split("\n")))
+        await locals()["_a_exec"]()
+        sys.stdout = orig
+    except Exception as err:
+        await i.response.followup(f"```py\n{err}\n```", view=view)
+    else:
+        value = stdout.getvalue()
+        if len(value) > 2000:
+            file = discohook.File("output.txt", content=value.encode("utf-8"))
+            return await i.response.followup(file=file, view=view)
+        embed = discohook.Embed("Output")
+        embed.set_author(name=str(i.author), icon_url=i.author.avatar.url)
+        embed.description = f"```\n{value}\n```"
+        await i.response.followup(embed=embed, view=view)
 
 
 @command_tree.message("exec")
