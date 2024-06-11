67bbd46b51cdce743bf4a43487d8a07f03a8a661
jnsougata <jnsougata@gmail.com>
2024-06-11 23:14:38 +0530

    Minor changes and bug fixes.

--- discohook/handler.py
+++ discohook/handler.py
@@ -43,13 +43,11 @@ async def _handler(request: Request):
     try:
         VerifyKey(public_key).verify(message, signature)
     except BadSignatureError:
-        await request.app.http.session.close()
         return Response(content="BadSignature", status_code=401)
     data = await request.json()
     interaction = Interaction(request.app, data)
     try:
         if interaction.type == InteractionType.ping:
-            await request.app.http.session.close()
             return JSONResponse({"type": InteractionCallbackType.pong}, status_code=200)
 
         elif interaction.type == InteractionType.app_command:
@@ -158,10 +156,11 @@ async def _handler(request: Request):
     except Exception as e:
         if request.app._interaction_error_handler:
             await request.app._interaction_error_handler(interaction, e)
-            await request.app.http.session.close()
             return Response(status_code=500)
         else:
             raise e from None
     else:
-        await request.app.http.session.close()
         return Response(status_code=200)
+    finally:
+        if request.app.http.session.closed:
+            await request.app.http.session.close()
