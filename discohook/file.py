5ef28c3c63237d47b61a1db3d9a892de3fa7341e
jnsougata <jnsougata@gmail.com>
2025-04-24 16:00:28 +0530

    feat: add from_path class method to create File object from file path

--- discohook/file.py
+++ discohook/file.py
@@ -29,3 +29,22 @@ class File:
         self.content = content
         self.spoiler = spoiler
         self.description = description
+
+    @classmethod
+    def from_path(cls, path: str, *, spoiler: bool = False, description: Optional[str] = None):
+        """
+        Creates a File object from a file path.
+
+        Parameters
+        ----------
+        path: str
+            The path to the file.
+        spoiler: bool
+            Whether the file is a spoiler.
+        description: str | None
+            The description of the file to be sent.
+        """
+        with open(path, "rb") as f:
+            content = f.read()
+        name = path.split("/")[-1]
+        return cls(name, content=content, spoiler=spoiler, description=description)
