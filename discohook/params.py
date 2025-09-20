9607831368198d2d4286fed673c6a7bd0893b6d4
jnsougata <jnsougata@gmail.com>
2025-09-20 16:09:20 +0530

    refactor: remove LegacyView class and its associated methods from params.py

--- discohook/params.py
+++ discohook/params.py
@@ -1,77 +1,15 @@
 import mimetypes
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, Optional, Union
 
 import aiohttp
 
-from .button import Button
-from .enums import ComponentType, InteractionCallbackType
+from .enums import InteractionCallbackType
 from .file import File
 from .models import AllowedMentions, MessageReference
-from .select import Select
 from .view import View
 
 if TYPE_CHECKING:
     pass
-
-
-class LegacyView:
-    """
-    Represents a discord message component tree.
-
-    This is used to create actions rows and add buttons and select menus to them without having tree conflicts.
-
-    Attributes
-    ----------
-    components: List[:class:`dict`]
-        The list of components to be sent to discord. Do not modify this directly.
-    children: List[Union[:class:`Button`, :class:`Select`]]
-        The list of children to be sent to discord. Do not modify this directly.
-    """
-
-    def __init__(self):
-        self.components: List[Dict[str, Any]] = []
-        self.children: List[Union[Button, Select]] = []
-
-    def add_buttons(self, *buttons: Union[Button, Any]):
-        """
-        Adds a row of buttons to the view.
-        Each row can only contain up to 5 buttons.
-        Action rows having buttons can not have select menus.
-
-        Parameters
-        ----------
-        *buttons: :class:`Button`
-            The buttons to be added to the view.
-        """
-        batches = [buttons[i : i + 5] for i in range(0, len(buttons), 5)]
-        for batch in batches:
-            self.components.append(
-                {
-                    "type": ComponentType.action_row,
-                    "components": [btn.to_dict() for btn in batch],
-                }
-            )
-            self.children.extend(batch)
-
-    # noinspection PyShadowingNames
-    def add_select(self, select: Union[Select, Any]):
-        """
-        Adds a row of select to the view.
-        Each row can only contain up to 1 select menu.
-        Action rows having select menu can not have buttons.
-
-        Parameters
-        ----------
-        select: :class:`Select`
-            The select menu to be added to the view.
-        """
-        self.components.append(
-            {
-                "type": ComponentType.action_row,
-                "components": [select.to_dict()],
-            }
-        )
-        self.children.append(select)
 
 
 def _append_file(form: aiohttp.MultipartWriter, index: int, file: File) -> None:
